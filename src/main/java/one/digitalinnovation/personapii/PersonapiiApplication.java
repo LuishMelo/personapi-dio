27772b14b15cba60fd3bf8be39468a0c7b4998cc
LuishMelo <luis.amaral@outlook.com>
2021-06-24 20:28:05 -0300

    First commit

---
+++ src/main/java/one/digitalinnovation/personapii/PersonapiiApplication.java
@@ -0,0 +1,13 @@
+package one.digitalinnovation.personapii;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonapiiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonapiiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/one/digitalinnovation/personapii/PersonapiiApplicationTests.java
@@ -0,0 +1,13 @@
+package one.digitalinnovation.personapii;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonapiiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
